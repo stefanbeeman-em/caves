bf331a1f70e5eff514c37920c042c97cf63fc101
Stefan Beeman <stefan@eldritchmedia.com>
2024-01-02 16:19:13 -0800

    Three.js is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,28 @@
-import { useState } from 'react'
-import { Text, View } from 'react-ape';
+import * as THREE from 'three';
+import React, { useRef, useState } from 'react'
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+
+function Box(props: ThreeElements['mesh']) {
+  const ref = useRef<THREE.Mesh>(null!)
+  const [hovered, hover] = useState(false)
+  const [clicked, click] = useState(false)
+  useFrame((state, delta) => (ref.current.rotation.x += delta))
+  return (
+    <mesh
+      {...props}
+      ref={ref}
+      scale={clicked ? 1.5 : 1}
+      onClick={(event) => click(!clicked)}
+      onPointerOver={(event) => hover(true)}
+      onPointerOut={(event) => hover(false)}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+    </mesh>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -27,15 +47,13 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        <View>
-          <Text>
-            Render this text on Canvas
-          </Text>
-          <Text>
-            You just use React Ape components like 'View' and 'Text',
-            just like React Native.
-          </Text>
-        </View>
+      <Canvas>
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+       </Canvas>
       </p>
     </>
   )
